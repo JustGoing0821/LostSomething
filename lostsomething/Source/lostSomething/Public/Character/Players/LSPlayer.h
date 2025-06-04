7f4a5089ab8f0bed3d03a00644a3c6070180614c
takealook2 <a1vm5h@gmail.com>
2025-06-04 22:19:45 +0900

    feat : LSPlayer 헤더 파일에 합체 튜토 인터페이스 함수 오버라이드

--- lostsomething/Source/lostSomething/Public/Character/Players/LSPlayer.h
+++ lostsomething/Source/lostSomething/Public/Character/Players/LSPlayer.h
@@ -20,6 +20,7 @@
 #include "InputAction.h"
 #include "Interface/LSWheelchairInterface.h"
 #include "LSPlayer.generated.h"
+#include "Interface/LSCombineTutorialInterface.h"
 
 class UInputAction;
 class UInputMappingContext;
@@ -30,7 +31,7 @@ class UInventoryWidget;
 
 
 UCLASS()
-class LOSTSOMETHING_API ALSPlayer : public ACharacter, public ILSTakeDamageInterface, public ILSWheelchairInterface
+class LOSTSOMETHING_API ALSPlayer : public ACharacter, public ILSTakeDamageInterface, public ILSWheelchairInterface, public ILSCombineTutorialInterface
 {
 	GENERATED_BODY()
 
@@ -48,6 +49,7 @@ public:
 	//Take Damage Section
 	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent, class AController* EventInstigator, AActor* DamageCauser) override;
 
+	virtual bool isCombining() override;
 
 	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
 
