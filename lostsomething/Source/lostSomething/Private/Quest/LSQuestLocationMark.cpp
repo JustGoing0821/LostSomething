d3a1bff70db46d06311dcfa7d0f7a1d71b32b665
LeeSeul <leeseul2021@duksung.ac.kr>
2025-06-30 21:48:15 +0900

    Feat : 퀘스트 위치 마크 액터 생성

---
+++ lostsomething/Source/lostSomething/Private/Quest/LSQuestLocationMark.cpp
@@ -0,0 +1,117 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Quest/LSQuestLocationMark.h"
+#include "lostSomething.h"
+#include "Net/UnrealNetwork.h"
+#include "NiagaraSystem.h"
+#include "NiagaraComponent.h"
+#include "NiagaraFunctionLibrary.h"
+#include "TimerManager.h"
+#include "Kismet/GameplayStatics.h"
+#include "GameFramework/GameModeBase.h"
+#include "Interface/LSQuestInterface.h"
+#include "Quest/LSQuestManager.h"
+
+// Sets default values
+ALSQuestLocationMark::ALSQuestLocationMark()
+{
+    bReplicates = true;
+}
+
+// Called when the game starts or when spawned
+void ALSQuestLocationMark::BeginPlay()
+{
+	Super::BeginPlay();
+
+
+    if (HasAuthority())
+    {
+        ILSQuestInterface* GameModeQuest = Cast<ILSQuestInterface>(UGameplayStatics::GetGameMode(GetWorld()));
+        if (GameModeQuest)
+        {
+            GameModeQuest->GetQuestManager()->OnQuestStart.AddUObject(this, &ALSQuestLocationMark::OnQuestChange);
+        }
+    }
+}
+
+void ALSQuestLocationMark::SpawnSystem()
+{
+    if (!LocationMark)
+    {
+        UE_LOG(LogTemp, Warning, TEXT("LocationMark is null!"));
+        return;
+    }
+
+    // Get Actor Location & Rotation
+    FVector ActorLocation = GetActorLocation();
+    FRotator ActorRotation = GetActorRotation();
+
+    // Spawn System at Location
+    UNiagaraComponent* SpawnedSystem = UNiagaraFunctionLibrary::SpawnSystemAtLocation(
+        this,
+        LocationMark,
+        ActorLocation,
+        ActorRotation,
+        FVector(Scale), // Scale을 FVector로 변환
+        true,  // Auto Destroy
+        true,  // Auto Activate
+        ENCPoolMethod::None,
+        true   // Pre Cull Check
+    );
+
+    if (SpawnedSystem)
+    {
+        // Set Niagara Variable (Float) - Scale 설정
+        SpawnedSystem->SetNiagaraVariableFloat(FString("Scale"), Scale);
+
+        // Branch 노드 구현 - Change Color 체크
+        if (bChangeColor)
+        {
+            // Set Niagara Variable (LinearColor) - MainColor 설정
+            SpawnedSystem->SetNiagaraVariableLinearColor(FString("MainColor"), CustomColor);
+        }
+    }
+}
+
+void ALSQuestLocationMark::OnQuestChange(FLSQuestData InQuestData, ELSInteractionEnum InQuestEnum)
+{
+    if (InQuestEnum == PuzzleActivateEnum)
+    {
+        MulticastRPCPuzzleActivate();
+    }
+    else
+    {
+        MulticastRPCPuzzleDeactivate();
+    }
+}
+
+void ALSQuestLocationMark::PuzzleActivate()
+{
+    // SpawnSystem 커스텀 이벤트 호출
+    SpawnSystem();
+
+    // Set Timer by Function Name 노드 구현
+    if (bIsLooping)
+    {
+        GetWorldTimerManager().SetTimer(SpawnTimerHandle, this, &ALSQuestLocationMark::SpawnSystem, LoopDuration, true);
+    }
+}
+
+void ALSQuestLocationMark::PuzzleDeactivate()
+{
+    if (GetWorld()->GetTimerManager().IsTimerActive(SpawnTimerHandle))
+    {
+        GetWorld()->GetTimerManager().ClearTimer(SpawnTimerHandle);
+    }
+}
+
+void ALSQuestLocationMark::MulticastRPCPuzzleActivate_Implementation()
+{
+    PuzzleActivate();
+}
+
+void ALSQuestLocationMark::MulticastRPCPuzzleDeactivate_Implementation()
+{
+    PuzzleDeactivate();
+}
