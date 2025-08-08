294d82c97e0b851aff846f88bafff12a88be01ea
LeeSeul <leeseul2021@duksung.ac.kr>
2025-08-09 01:43:34 +0900

    Feat : 맵 버전관리용 액터 생성

---
+++ lostsomething/Source/lostSomething/Private/Level/LSMapVersionControll.cpp
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Level/LSMapVersionControll.h"
+
+// Sets default values
+ALSMapVersionControll::ALSMapVersionControll()
+{
+	CurrentMapVersion = 0;
+}
+
+// Called when the game starts or when spawned
+void ALSMapVersionControll::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
