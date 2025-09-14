85accbc06b2fb9a637f08561a871ed26438b1340
LeeSeul <leeseul2021@duksung.ac.kr>
2025-09-15 04:58:27 +0900

    Fix : 자잘하게 추가수정

--- lostsomething/Source/lostSomething/lostSomething.cpp
+++ lostsomething/Source/lostSomething/lostSomething.cpp
@@ -4,4 +4,5 @@
 #include "Modules/ModuleManager.h"
 
 DEFINE_LOG_CATEGORY(LogLS);
+DEFINE_LOG_CATEGORY(LogLSls);
 IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, lostSomething, "lostSomething" );
