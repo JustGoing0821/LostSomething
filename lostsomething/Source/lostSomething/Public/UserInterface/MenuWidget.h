d324df6ee8bdd6764b6648282fa83bba4bd83c17
LeeSeul <127869350+LeeSeul0473@users.noreply.github.com>
2025-12-02 13:39:46 +0900

    Feat : 메뉴 위젯 추가, 미니맵 수정 -1

--- lostsomething/Source/lostSomething/Public/UserInterface/MenuWidget.h
+++ lostsomething/Source/lostSomething/Public/UserInterface/MenuWidget.h
@@ -13,5 +13,38 @@ UCLASS()
 class LOSTSOMETHING_API UMenuWidget : public UUserWidget
 {
 	GENERATED_BODY()
+
+public:
+	//=============================================
+	//     PROPERTIES & VARIABLES
+	//=============================================
+
+	UPROPERTY(EditDefaultsOnly, meta = (BindWidget))
+	class UButton* btn_Continue;
+
+	UPROPERTY(EditDefaultsOnly, meta = (BindWidget))
+	class UButton* btn_Lobby;
+
+	UPROPERTY(EditDefaultsOnly, meta = (BindWidget))
+	class UButton* btn_ChooseMap;
+
+	UPROPERTY(EditDefaultsOnly, meta = (BindWidget))
+	class UButton* btn_Quit;
+
+	//=============================================
+	//     FUCTIONS
+	//=============================================
+	virtual void NativeConstruct() override;
 	
+	UFUNCTION(BlueprintCallable)
+	void ContinueGame();
+
+	UFUNCTION(BlueprintCallable)
+	void GoLobby();
+
+	UFUNCTION(BlueprintCallable)
+	void GoChooseMap();
+
+	UFUNCTION(BlueprintCallable)
+	void QuitGame();
 };
